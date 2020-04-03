32cd982b95402a1da5f9851d22fe6d736f39cfec
Rui Lebre <ruilebre@ua.pt>
2020-04-03 18:58:17 +0100

    added subscribe and unsubscribe services

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Store = require('../models/store');
 const { normalizeErrors } = require('../helpers/mongoose');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
@@ -91,28 +92,74 @@ exports.authMiddleware = function (req, res, next) {
     }
 }
 
+exports.getUserDetails = function (req, res) {
+    return res.json({ 'user': res.locals.user });
+}
+
+exports.subscribe = function (req, res) {
+    const { store } = req.body;
+    const user = res.locals.user;
+
+    Store.findById(store._id)
+        .populate('usersSubscribed')
+        .exec(function (err, foundStore) {
+            if (err) {
+                console.log(err);
+                return res.status(422).send({ errors: normalizeErrors(err.errors) });
+            }
+
+            if (!foundStore) {
+                return res.status(422).send({ errors: [{ title: "Invalid store!", detail: "Store does not exist." }] });
+            }
+
+            if (foundStore.usersSubscribed.find(subscribed => subscribed._id.equals(user._id)) &&
+                user.storeSubscriptions.find(subscription => subscription._id.equals(foundStore._id))) {
+                return res.status(422).send({ errors: [{ title: "Already subscribed!", detail: "User already subscribed this store." }] });
+            }
+
+            foundStore.usersSubscribed.push(user);
+            foundStore.save();
+            user.storeSubscriptions.push(foundStore);
+            user.save();
+
+            return res.json({ 'status': 'subscribed' });
+        });
+}
+
+exports.unsubscribe = function (req, res) {
+    const { store } = req.body;
+    const user = res.locals.user;
+
+    Store.findById(store._id)
+        .populate('usersSubscribed')
+        .exec(function (err, foundStore) {
+            if (err) {
+                console.log(err);
+                return res.status(422).send({ errors: normalizeErrors(err.errors) });
+            }
+
+            if (!foundStore) {
+                return res.status(422).send({ errors: [{ title: "Invalid store!", detail: "Store does not exist." }] });
+            }
+
+            if (!foundStore.usersSubscribed.find(subscribed => subscribed._id.equals(user._id)) ||
+                !user.storeSubscriptions.find(subscription => subscription._id.equals(foundStore._id))) {
+                return res.status(422).send({ errors: [{ title: "No subscription found!", detail: "User has no subscription in this store." }] });
+            }
+
+            foundStore.usersSubscribed.pull(user);
+            foundStore.save();
+            user.storeSubscriptions.pull(foundStore);
+            user.save();
+
+            return res.json({ 'status': 'unsubscribed' });
+        });
+}
+
 function parseToken(token) {
     return jwt.verify(token.split(' ')[1], config.SECRET);
 }
 
 function notAuthorized(res) {
     return res.status(401).send({ errors: [{ title: "Not authorized.", detail: "You need to login" }] });
-}
-
-exports.getUserDetails = function (req, res) {
-    const username = req.query.username;
-
-    User.findOne({ username })
-        .exec(function (err, existingUser) {
-            if (err) {
-                console.log(err);
-                return res.status(422).send({ errors: normalizeErrors(err.errors) });
-            }
-
-            if (!existingUser) {
-                return res.status(422).send({ errors: [{ title: "Invalid user!", detail: "User does not exist." }] });
-            }
-
-            return res.json({ 'user': existingUser });
-        });
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,9 @@ const User = require("../controllers/user");
 
 router.post("/auth", User.auth);
 router.post("/register", User.register);
+
 router.get("/get-user-details", User.authMiddleware, User.getUserDetails);
+router.post("/subscribe", User.authMiddleware, User.subscribe);
+router.post("/unsubscribe", User.authMiddleware, User.unsubscribe);
 
 module.exports = router;
