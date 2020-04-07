8fce093bc0c4a1d1b2773514b1ae1e646c070456
Rui Lebre <ruilebre@ua.pt>
2020-04-07 17:06:48 +0100

    added login

--- android/app/src/main/java/com/ruilebre/pandemicqueue/ui/login/LoginActivity.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/ui/login/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.ruilebre.pandemicqueue.ui.login;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -12,27 +14,88 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.ruilebre.pandemicqueue.R;
+import com.ruilebre.pandemicqueue.data.NormalizedError;
 import com.ruilebre.pandemicqueue.data.models.SessionToken;
-import com.ruilebre.pandemicqueue.services.user.Auth;
+import com.ruilebre.pandemicqueue.utils.Endpoint;
+import com.ruilebre.pandemicqueue.utils.backendendpoints.UserEndpoint;
 
-import java.util.concurrent.ExecutionException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Endpoint authEndpoint;
+    private LoginViewModel loginViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        try {
+            this.authEndpoint = new Endpoint("http://192.168.1.93", "3000", Endpoint.DEFAULT_API + UserEndpoint.AUTH);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        loginViewModel = new LoginViewModel(this);
+
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            @Override
+            public void onChanged(@Nullable LoginResult loginResult) {
+                if (loginResult == null) {
+                    return;
+                }
+                loadingProgressBar.setVisibility(View.GONE);
+                if (loginResult.getError() != null) {
+                    showLoginFailed(loginResult.getError());
+                }
+                if (loginResult.getSuccess() != false) {
+                    updateUiWithUser("asdasdas");
+                }
+                setResult(Activity.RESULT_OK);
+
+                //Complete and destroy login activity once successful
+                finish();
+            }
+        });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -47,6 +110,7 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(usernameEditText.getText().toString(), passwordEditText.getText().toString());
             }
         };
         usernameEditText.addTextChangedListener(afterTextChangedListener);
@@ -56,7 +120,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-
+                    loginViewModel.login(authEndpoint, usernameEditText.getText().toString(), passwordEditText.getText().toString());
                 }
                 return false;
             }
@@ -66,26 +130,65 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 loadingProgressBar.setVisibility(View.VISIBLE);
+                loginViewModel.login(authEndpoint, usernameEditText.getText().toString(), passwordEditText.getText().toString());
             }
         });
+/*
+        passwordEditText.setOnEditorActionListener(textListener);
+        usernameEditText.setOnEditorActionListener(textListener);
 
-        String body = "{\"email\": \"test6@test.com\",\"password\": \"1234\"}";
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performAuthentication(authEndpoint, usernameEditText.getText().toString(), passwordEditText.getText().toString());
+                loadingProgressBar.setVisibility(View.VISIBLE);
+            }
+        });*/
+    }
+
+    private void performAuthentication(Endpoint authEndpoint, String email, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject jsonBody = new JSONObject();
         try {
-            System.out.println("******************");
-            SessionToken token = new Auth("http://192.168.1.93:3000/api/v1/users/auth", "POST", body).getToken();
-            System.out.println(token);
-            System.out.println("******************");
-
-        } catch (ExecutionException | InterruptedException e) {
+            jsonBody.put("email", email);
+            jsonBody.put("password", password);
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, authEndpoint.toString(), jsonBody, new Response.Listener() {
+            @Override
+            public void onResponse(Object res) {
+                JSONObject response = (JSONObject) res;
+                Log.i("VOLLEY", response.toString());
+                SessionToken token = new Gson().fromJson(response.toString(), SessionToken.class);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+                try {
+                    JSONObject jsonError = new JSONObject(new String(error.networkResponse.data, "UTF-8"));
+                    jsonError = jsonError.getJSONArray("errors").getJSONObject(0);
+
+                    NormalizedError normalizedError = new Gson().fromJson(jsonError.toString(), NormalizedError.class);
+
+                    System.out.println(normalizedError);
+                } catch (UnsupportedEncodingException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
+    private void updateUiWithUser(String model) {
+        String welcome = getString(R.string.welcome) + model;
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
+
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
