64537677820fd4fa127383cc88f1f0035e040097
Rui Lebre <ruilebre@ua.pt>
2020-04-07 17:05:32 +0100

    fixed users endpoints

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/UserEndpoint.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/UserEndpoint.java
@@ -1,7 +1,7 @@
 package com.ruilebre.pandemicqueue.utils.backendendpoints;
 
 public class UserEndpoint {
-    public static final String AUTH = "/auth";
-    public static final String REGISTER = "/register";
-    public static final String GET_USER_DETAILS = "/get-user-details";
+    public static final String AUTH = "/users/auth";
+    public static final String REGISTER = "/users/register";
+    public static final String GET_USER_DETAILS = "/users/get-user-details";
 }
