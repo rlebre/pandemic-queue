9503cb883195b7f90481661f4e3853f81ea5c9af
Rui Lebre <ruilebre@ua.pt>
2020-04-07 17:06:31 +0100

    added normalized error class

--- android/app/src/main/java/com/ruilebre/pandemicqueue/data/NormalizedError.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/data/NormalizedError.java
@@ -1,5 +1,30 @@
 package com.ruilebre.pandemicqueue.data;
 
 public class NormalizedError {
+    private String title;
+    private String detail;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "NormalizedError{" +
+                "title='" + title + '\'' +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
 }
