33ae4f9c5423b4dc5fffe49d4169a389ad3a0c1c
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:39:07 +0100

    updated store endpoints

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/StoreEndpoint.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/StoreEndpoint.java
@@ -1,8 +1,10 @@
 package com.ruilebre.pandemicqueue.utils.backendendpoints;
 
+import com.ruilebre.pandemicqueue.utils.Endpoint;
+
 public class StoreEndpoint {
-    public static final String POST_STORE = "/store";
-    public static final String GET_NUMBER_TICKETS = "/number-waiting-tickets";
-    public static final String GET_WAITING_TICKETS = "/waiting-tickets";
-    public static final String GET_STORE_DETAILS = "/get-store-details";
+    public static final String POST_STORE = Endpoint.API_BASEPATH + "/stores/store";
+    public static final String GET_NUMBER_TICKETS = Endpoint.API_BASEPATH + "/stores/number-waiting-tickets";
+    public static final String GET_WAITING_TICKETS = Endpoint.API_BASEPATH + "/stores/waiting-tickets";
+    public static final String GET_STORE_DETAILS = Endpoint.API_BASEPATH + "/stores/get-store-details";
 }
