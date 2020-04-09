a15ab06c93922a09af0be3edf0017f32de7bd1a4
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:39:33 +0100

    created status object

--- android/app/src/main/java/com/ruilebre/pandemicqueue/data/Status.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/data/Status.java
@@ -1,4 +1,13 @@
 package com.ruilebre.pandemicqueue.data;
 
 public class Status {
+    private String registered;
+
+    public String getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(String registered) {
+        this.registered = registered;
+    }
 }
