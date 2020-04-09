482a96d155e705458a62df08d7279dc57111c388
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:38:47 +0100

    created subscription service definition

--- android/app/src/main/java/com/ruilebre/pandemicqueue/services/SubscriptionService.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/services/SubscriptionService.java
@@ -1,19 +1,18 @@
 package com.ruilebre.pandemicqueue.services;
 
-import com.ruilebre.pandemicqueue.data.Status;
 import com.ruilebre.pandemicqueue.data.models.Store;
-import com.ruilebre.pandemicqueue.utils.backendendpoints.TicketEndpoint;
+import com.ruilebre.pandemicqueue.utils.backendendpoints.SubscriptionEndpoint;
+
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface SubscriptionService {
+    @POST(SubscriptionEndpoint.SUBSCRIBE)
+    Call<HashMap<String, String>> subscribe(@Body Store body);
 
-    @POST(TicketEndpoint.CALL_TICKET)
-    Call<Status> callTicket(@Body Store body);
-
-    @POST(TicketEndpoint.CREATE_TICKET)
-    Call<Status> createTicket(@Body Store store);
-
+    @POST(SubscriptionEndpoint.UNSUBSCRIBE)
+    Call<HashMap<String, String>> unsubscribe(@Body Store store);
 }
