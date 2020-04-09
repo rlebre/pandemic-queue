54d29cf6cfde50f831451f55f94db5fc24a245a5
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:40:59 +0100

    declared store api

--- android/app/src/main/java/com/ruilebre/pandemicqueue/services/ApiUtils.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/services/ApiUtils.java
@@ -7,4 +7,16 @@ public class ApiUtils {
     public static UserService getUserService() {
         return RetrofitClient.getRetrofit(BASE_URL).create(UserService.class);
     }
+
+    public static StoreService getStoreService() {
+        return RetrofitClient.getRetrofit(BASE_URL).create(StoreService.class);
+    }
+
+    public static TicketService getTicketService() {
+        return RetrofitClient.getRetrofit(BASE_URL).create(TicketService.class);
+    }
+
+    public static SubscriptionService getSubscriptionService() {
+        return RetrofitClient.getRetrofit(BASE_URL).create(SubscriptionService.class);
+    }
 }
