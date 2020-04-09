51862d163cea7a9973cd62dfb4caa83fcd24a758
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:39:23 +0100

    updated ticket endpoints

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/TicketEndpoint.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/TicketEndpoint.java
@@ -1,6 +1,8 @@
 package com.ruilebre.pandemicqueue.utils.backendendpoints;
 
+import com.ruilebre.pandemicqueue.utils.Endpoint;
+
 public class TicketEndpoint {
-    public static final String CREATE_TICKET = "/ticket";
-    public static final String CALL_TICKET = "/call-ticket";
+    public static final String CREATE_TICKET = Endpoint.API_BASEPATH + "/tickets/ticket";
+    public static final String CALL_TICKET = Endpoint.API_BASEPATH + "/tickets/call-ticket";
 }
