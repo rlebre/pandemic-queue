3e1ffeccd0ae8eb51173f4cad5fba36bfc5cc25e
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:38:54 +0100

    created Ticket service definition

--- android/app/src/main/java/com/ruilebre/pandemicqueue/services/TicketService.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/services/TicketService.java
@@ -1,30 +1,17 @@
 package com.ruilebre.pandemicqueue.services;
 
-import com.ruilebre.pandemicqueue.data.models.SessionToken;
+import com.ruilebre.pandemicqueue.data.Status;
 import com.ruilebre.pandemicqueue.data.models.Store;
-import com.ruilebre.pandemicqueue.data.models.Ticket;
-import com.ruilebre.pandemicqueue.utils.backendendpoints.StoreEndpoint;
-
-import java.util.HashMap;
-import java.util.List;
+import com.ruilebre.pandemicqueue.utils.backendendpoints.TicketEndpoint;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface TicketService {
+    @POST(TicketEndpoint.CALL_TICKET)
+    Call<Status> callTicket(@Body Store body);
 
-    @POST(StoreEndpoint.POST_STORE)
-    Call<SessionToken> login(@Body SessionToken body);
-
-    @GET(StoreEndpoint.GET_NUMBER_TICKETS)
-    Call<HashMap<String, String>> getNumberTicketsWaiting(@Query("store") String store);
-
-    @GET(StoreEndpoint.GET_WAITING_TICKETS)
-    Call<List<Ticket>> getWaitingTickets(@Query("store") String store);
-
-    @GET(StoreEndpoint.GET_STORE_DETAILS)
-    Call<Store> getStoreDetails(@Query("store") String store);
+    @POST(TicketEndpoint.CREATE_TICKET)
+    Call<Status> createTicket(@Body Store store);
 }
