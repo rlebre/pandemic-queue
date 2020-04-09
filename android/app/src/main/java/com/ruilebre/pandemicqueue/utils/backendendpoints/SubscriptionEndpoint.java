a5de627a5d05087e630fb4c622b2bb5dbe5f4765
Rui Lebre <ruilebre@ua.pt>
2020-04-09 16:39:16 +0100

    updated subscription endpoints

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/SubscriptionEndpoint.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/backendendpoints/SubscriptionEndpoint.java
@@ -1,6 +1,8 @@
 package com.ruilebre.pandemicqueue.utils.backendendpoints;
 
+import com.ruilebre.pandemicqueue.utils.Endpoint;
+
 public class SubscriptionEndpoint {
-    public static final String SUBSCRIBE = "/subscribe";
-    public static final String UNSUBSCRIBE = "/unsubscribe";
+    public static final String SUBSCRIBE = Endpoint.API_BASEPATH + "/subscriptions/subscribe";
+    public static final String UNSUBSCRIBE = Endpoint.API_BASEPATH + "/subscriptions/unsubscribe";
 }
