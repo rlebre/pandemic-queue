8e03d93c6c191842108bd30fe662518463a7536b
Rui Lebre <ruilebre@ua.pt>
2020-04-11 12:55:35 +0100

    reverted to develop IP address

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/ApiUtils.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/ApiUtils.java
@@ -7,7 +7,7 @@ import com.ruilebre.pandemicqueue.services.UserService;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://94.61.244.169:3000";
+    public static final String BASE_URL = "http://192.168.1.93:3000";
 
     public static UserService getUserService() {
         return RetrofitClient.getRetrofit(BASE_URL).create(UserService.class);
