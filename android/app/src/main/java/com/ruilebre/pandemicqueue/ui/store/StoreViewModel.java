5e6639a26649af59b44cbdd3db3ea4587f593cfe
Rui Lebre <ruilebre@ua.pt>
2020-04-13 13:04:25 +0100

    added storeViewModel

---
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/ui/store/StoreViewModel.java
@@ -0,0 +1,63 @@
+package com.ruilebre.pandemicqueue.ui.storelist;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.gson.Gson;
+import com.ruilebre.pandemicqueue.data.NormalizedError;
+import com.ruilebre.pandemicqueue.data.models.Store;
+import com.ruilebre.pandemicqueue.services.StoreService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+
+public class StoreListViewModel extends ViewModel {
+    private StoreService storeService;
+    private MutableLiveData<List<Store>> storeListResult = new MutableLiveData<>();
+
+    StoreListViewModel(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    LiveData<List<Store>> getStoreListResult() {
+        return storeListResult;
+    }
+
+    public void getStoreList() {
+        Call call = storeService.getStoreList();
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, retrofit2.Response response) {
+                if (response.isSuccessful()) {
+                    List<Store> storeList = (List<Store>) response.body();
+
+                    storeListResult.setValue(storeList);
+                } else {
+                    try {
+                        JSONObject jsonError = new JSONObject(response.errorBody().string());
+                        jsonError = jsonError.getJSONArray("errors").getJSONObject(0);
+                        NormalizedError normalizedError = new Gson().fromJson(jsonError.toString(), NormalizedError.class);
+                        Log.e("FETCH", normalizedError.toString());
+                    } catch (JSONException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+
+            }
+        });
+    }
+}
