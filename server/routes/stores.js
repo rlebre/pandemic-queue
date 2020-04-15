408fc0f4b91016e8fca20030ba3b2ddbe3c43b47
Rui Lebre <ruilebre@ua.pt>
2020-04-15 23:38:59 +0100

    changed create store method

--- server/routes/stores.js
+++ server/routes/stores.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Store = require("../controllers/store");
 
-router.post("/store", Store.createStore);
+router.post("/create", Store.createStore);
 router.get("/number-waiting-tickets", Store.getNumberStoreWaitingTickets);
 router.get("/waiting-tickets", Store.getStoreWaitingTickets);
 router.get("/get-store-details", Store.getStoreDetails);
