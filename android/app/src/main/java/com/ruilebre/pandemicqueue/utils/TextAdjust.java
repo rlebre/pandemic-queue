ffe6b412f2dffb856c7c2f3295704c1a25fbf6f0
Rui Lebre <ruilebre@ua.pt>
2020-04-20 23:58:00 +0100

    added title case after ( and )

--- android/app/src/main/java/com/ruilebre/pandemicqueue/utils/TextAdjust.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/utils/TextAdjust.java
@@ -14,12 +14,11 @@ public class TextAdjust {
         for (int i = 0; i < len; ++i) {
             char c = builder.charAt(i);
             if (space) {
-                if (!Character.isWhitespace(c)) {
-                    // Convert to title case and switch out of whitespace mode.
+                if (!Character.isWhitespace(c) && c != '(' && c != ')') {
                     builder.setCharAt(i, Character.toTitleCase(c));
                     space = false;
                 }
-            } else if (Character.isWhitespace(c)) {
+            } else if (Character.isWhitespace(c) || c == '(' || c == ')') {
                 space = true;
             } else {
                 builder.setCharAt(i, Character.toLowerCase(c));
