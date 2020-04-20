7540af939475125f3272203753859e02dcd95b28
Rui Lebre <ruilebre@ua.pt>
2020-04-20 17:42:31 +0100

    set toolbar content

--- android/app/src/main/java/com/ruilebre/pandemicqueue/ui/MainActivity.java
+++ android/app/src/main/java/com/ruilebre/pandemicqueue/ui/MainActivity.java
@@ -1,15 +1,26 @@
 package com.ruilebre.pandemicqueue.ui;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.ruilebre.pandemicqueue.R;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Pandemic Queue");
     }
 }
